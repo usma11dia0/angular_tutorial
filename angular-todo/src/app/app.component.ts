859d8b9408a9df3f2c0efd7dbb8e347b5ec584a3
usma11dia0 <usma11dia0@gmail.com>
2023-03-18 22:30:01 +0900

    chmod angular-todo

--- angular-todo/src/app/app.component.ts
+++ angular-todo/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo';
+  tasks = [
+    {title: '牛乳を買う', done: false},
+    {title: '可燃ごみを出す', done: true},
+    {title: '銀行に行く', done: false},
+  ]
 }
