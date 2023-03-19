20554ddc6a3d00b9f66241760f39527f7e14163f
usma11dia0 <usma11dia0@gmail.com>
2023-03-19 05:56:55 +0000

    add overdue function to component

--- angular-todo/src/app/app.component.ts
+++ angular-todo/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  title: string;
+  done: boolean;
+  deadline: Date;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +14,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-todo';
   tasks = [
-    {title: '牛乳を買う', done: false},
-    {title: '可燃ごみを出す', done: true},
-    {title: '銀行に行く', done: false},
+    {title: '牛乳を買う', done: false, deadline: new Date('2021-01-01')},
+    {title: '可燃ゴミを出す', done: true, deadline: new Date('2020-01-02')},
+    {title: '銀行に行く', done: false, deadline: new Date('2020-01-03')},
   ];
 
-  newTaskTitle = '';
+  newTask = {
+    title: '',
+    deadline: new Date(),
+  };
+  
+  addTask() {
+    this.tasks.push({title: this.newTask.title, done: false, deadline: new Date(this.newTask.deadline)});
+    this.newTask = {
+      title: '',
+      deadline: new Date(),
+    };
+  };
+
+  isOverdue(task:Task) {
+    return !task.done && task.deadline < new Date();
+  }
 }
